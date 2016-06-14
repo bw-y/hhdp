a9f89951a84051161470442cd1e0e8ca59545b8b
bw.y <i@bw-y.com>
2016-06-14 23:45:51 +0800

    init 1

--- hhdp.py
+++ hhdp.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import os
 import sys
 
 
+class DoIt(object):
+    def __init__(self, hosts_file, args):
+        self.hosts = hosts_file
+        self.args = args
+        self.__args_check()
+
+    def __args_check(self):
+        if not os.path.isfile(self.hosts):
+            sys.stdout.write("%s no such file\n" % self.hosts)
+            sys.exit(1)
+
+    def run(self):
+        print(self.args)
+        print(self.hosts)
+
 if __name__ == '__main__':
-    print(sys.argv)
-    
+    instance = DoIt('/etc/hosts_list', sys.argv)
+    instance.run()
